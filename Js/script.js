b23d7cbd58babe6cd79e05f62a562d95da56af82
bguzman012 <46763361+bguzman012@users.noreply.github.com>
2020-04-08 15:29:41 -0500

    casi casi casi paginacion 5

--- Js/script.js
+++ Js/script.js
@@ -1,73 +1,84 @@
 var req = new XMLHttpRequest();
+var arreglo = new Array();
 function bus() {
 
     var prueba;
     var result = "";
 
 
+
     var porNombre = document.getElementsByName("q")[0].value;
     var numero_pag = document.getElementById("paginas").value;
-    //document.getElementById("resultado").innerHTML=porNombre;
-    // Creación de la petición HTTP
-
-    // Petición HTTP GET síncrona hacia el archivo fotos.json del servidor
-    req.open("GET", "http://www.omdbapi.com/?apikey=eabd474&s=" + porNombre + "&page=" + numero_pag, false);
-    // Envío de la petición
-    req.send(null);
-
-
-    var data = JSON.parse(req.responseText)
-
-    console.log(data)
+    document.getElementById('action').innerHTML = "";
+    var intro = document.getElementById('mia');
+    intro.style.visibility = "visible";
 
     var detalles = "";
 
+    for (i = 1; i <= 100; i++) {
+        // Petición HTTP GET síncrona hacia el archivo fotos.json del servidor
+        req.open("GET", "http://www.omdbapi.com/?apikey=eabd474&s=" + porNombre + "&page=" + i, false);
+        // Envío de la petición
+        req.send(null);
+        data = JSON.parse(req.responseText)
+        var jj = data.Response;
+        
+        if (jj == "True") {
 
-    if (porNombre == "") {
-        detalles = "<tr>" +
-            "<td colspan='5'> Sin informacion disponible...</td>" +
-            "</tr>";
-        document.getElementById("informacion").innerHTML = detalles;
-    } else {
-        data.Search.forEach(movie => {
-            detalles += "<tr>" +
-                "<td><a href='#' onclick=\"buscarPorID('" + movie.imdbID + "')\">Mas detalles </td>" +
-                "<td>" + movie.Title + "</td>" +
-                "<td>" + movie.Year + "</td>" +
-                "<td>" + movie.Type + "</td>" +
-                "<td><img src=" + movie.Poster + "</td>" +
-                "</tr>";
-            console.log(movie.imdbID)
-        });
-        document.getElementById("informacion").innerHTML = detalles;
-        document.getElementById('action').innerHTML = "";
+            if (porNombre == "") {
+                detalles = "<tr>" +
+                    "<td colspan='5'> Sin informacion disponible...</td>" +
+                    "</tr>";
+                document.getElementById("informacion").innerHTML = detalles;
+            } else {
+                data.Search.forEach(movie => {
+                    detalles += "<tr>" +
+                        "<td><a href='#' onclick=\"buscarPorID('" + movie.imdbID + "')\">Mas detalles </td>" +
+                        "<td>" + movie.Title + "</td>" +
+                        "<td>" + movie.Year + "</td>" +
+                        "<td>" + movie.Type + "</td>" +
+                        "<td><img src=" + movie.Poster + "</td>" +
+                        "</tr>";
+                        
+                });
+            }
+            document.getElementById("informacion").innerHTML = detalles;
+
+        } else {
+            $(document).ready(function () {
+
+                var rowsShown = 5;
+                var rowsTotal = $('#mia tbody tr').length;
+                console.log($('#mia tbody tr').length);
+                var numPages = rowsTotal / rowsShown;
+                var indices = "";
+                for (i = 0; i < numPages; i++) {
+                    var pageNum = i + 1;
+                    indices += '<a href="#" rel="' + i + '">' + pageNum + '</a> ';
+                };
+                document.getElementById('menu').innerHTML = indices;
+                $('#mia tbody tr').hide();
+                $('#mia tbody tr').slice(0, rowsShown).show();
+                $('#menu a:first').addClass('active');
+                $('#menu a').bind('click', function () {
+                    $('#menu a').removeClass('active');
+                    $(this).addClass('active');
+                    var currPage = $(this).attr('rel');
+                    var startItem = currPage * rowsShown;
+                    var endItem = startItem + rowsShown;
+                    $('#mia tbody tr').css('opacity', '0.0').hide().slice(startItem, endItem).
+                        css('display', 'table-row').animate({ opacity: 1 }, 300);
+                });
+        
+            });
+
+            return;
+        }
     }
 
-    $(document).ready(function () {
 
-        var rowsShown = 5;
-        var rowsTotal = $('#mia tbody tr').length;
-        var numPages = rowsTotal / rowsShown;
-        var indices = "";
-        for (i = 0; i < numPages; i++) {
-            var pageNum = i + 1;
-            indices += '<a href="#" rel="' + i + '">' + pageNum + '</a> ';
-        };
-        document.getElementById('menu').innerHTML = indices;
-        $('#mia tbody tr').hide();
-        $('#mia tbody tr').slice(0, rowsShown).show();
-        $('#menu a:first').addClass('active');
-        $('#menu a').bind('click', function () {
-            $('#menu a').removeClass('active');
-            $(this).addClass('active');
-            var currPage = $(this).attr('rel');
-            var startItem = currPage * rowsShown;
-            var endItem = startItem + rowsShown;
-            $('#mia tbody tr').css('opacity', '0.0').hide().slice(startItem, endItem).
-                css('display', 'table-row').animate({ opacity: 1 }, 300);
-        });
 
-    });
+    
 }
 
 function buscarPorID(clave) {
